1d12dae291ed8d98090cbf6cfda17b803c16545b
zhangliangzhi <521401@qq.com>
2019-05-13 20:01:22 +0800

    use typescript koa2 typeorm init

---
+++ README.md
@@ -0,0 +1,8 @@
+# use typescript koa2 typeorm init.
+# Awesome Project Build with TypeORM
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `ormconfig.json` file
+3. Run `npm start` command
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+import * as Koa from 'koa';
+
+function createDbConn(){
+    createConnection().then(async connection => {
+
+        console.log("Inserting a new user into the database...");
+        const user = new User();
+        user.firstName = "Timber";
+        user.lastName = "Saw";
+        user.age = 25;
+        user.dollar = 888;
+        await connection.manager.save(user);
+        console.log("Saved a new user with id: " + user.id);
+    
+        console.log("Loading users from the database...");
+        const users = await connection.manager.find(User);
+        console.log("Loaded users: ", users);
+    
+        console.log("Here you can setup and run express/koa/any other framework.");
+    
+    }).catch(error => console.log(error));
+}
+
+createDbConn();
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'hello koa2 typescript';
+
+})
+const server = app.listen(3000, () => {
+    console.log('---Server is running at http://localhost:3000');
+    console.log('---Press CTRL-C to stop \n');
+});
+
+
